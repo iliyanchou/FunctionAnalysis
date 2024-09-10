516c7b3167d840368811c863c63dce889a978229
Ilia Granchulov <iliya0620@outlook.com>
2024-09-11 01:05:18 +0300

    Adding language supp

---
+++ Static/Js/langDiff.js
@@ -0,0 +1,21 @@
+function language(){
+    let lang = 'bg';
+   if(lang === 'en'){
+     document.getElementById('page-title-diff').innerHTML = "Differentiation"
+   }
+   if(lang === 'bg'){
+     document.getElementById('page-title-diff').innerHTML = "Производна на функция"
+     document.getElementById('title-diff').innerHTML = "Производна на функция"
+     document.getElementById('diff-anc-1').innerHTML = "Главна страница"
+     document.getElementById('diff-anc-2').innerHTML = "Калкулатор за квадратни уравнения"
+     document.getElementById('insf-diff').innerHTML = "Въведете функция"
+     document.getElementById('shortDesc-diff').innerHTML = "Използвайте стандартно означение като например '^', 'sqrt' за корен квадратен , всички аргументи на функции трябва да бъдат в скоби!"
+     document.getElementsByName('functionInput')[0].placeholder = "Функция"
+     document.getElementsByName('functionVar')[0].placeholder = "Спрямо?"
+     document.getElementById('diff-btn').innerHTML = "Диференцирай"
+     document.getElementById('shortDesc-diff_2').innerHTML = "Производната на функцията е:"
+
+
+
+   }
+   }
---
+++ Static/Js/langIndex.js
@@ -0,0 +1,16 @@
+function language(){
+ let lang = document.getElementById('lang-select').value;
+if(lang === 'en'){
+  document.getElementById('page-title-index').innerHTML = "Math Analysis"
+  document.getElementById('index-title').innerHTML = "Math Analysis"
+  document.getElementById('index-anc-1').innerHTML = "Differentiation"
+  document.getElementById('index-anc-2').innerHTML = "Quadratic solver"
+}
+if(lang === 'bg'){
+  document.getElementById('page-title-index').innerHTML = "Математически Анализ"
+  document.getElementById('index-title').innerHTML = "Математически Анализ"
+  document.getElementById('index-anc-1').innerHTML = "Производна на функция"
+  document.getElementById('index-anc-2').innerHTML = "Калкулатор за квадратни уравнения"
+}
+}
+
